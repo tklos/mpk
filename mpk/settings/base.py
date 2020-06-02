b0338d3b7fb24ad908c2c85852f4438f57640391
tklos <tomasz.klos9@gmail.com>
2020-06-02 12:13:11 +0200

    Use absolute paths

--- mpk/settings/base.py
+++ mpk/settings/base.py
@@ -132,13 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../public/static/')
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../public/static/'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../public/media/')
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../public/media/'))
 
 
 # Timezone
